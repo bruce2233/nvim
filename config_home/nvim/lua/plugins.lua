f7d97e52b5b8d20a5022fa251031e2d3679e3a74
LAPTOP-N1195GTQ\86442 <864427717@qq.com>
2022-08-27 20:06:43 +0800

    create plugins.lua

---
+++ config_home/nvim/lua/plugins.lua
@@ -0,0 +1,17 @@
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icons
+    }
+  }
+  use 'glepnir/zephyr-nvim'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use 'navarasu/onedark.nvim'
+  use 'glepnir/dashboard-nvim'
+end)
